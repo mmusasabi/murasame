ba003f09ffb37b1c7a61953e14143bcde6643864
zaru <sakuraba@basicinc.jp>
2017-06-01 16:06:26 +0900

    Added console.vue

---
+++ app/client/src/Console.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="console">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'console',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ app/client/src/console.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Console from './Console.vue'
+
+new Vue({
+  el: '#console',
+  render: h => h(Console)
+})
